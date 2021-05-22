dbc088ce565ff5d57610879d8f995dbc7af925f0
ZnakZorro <znakzorro@gmail.com>
2021-05-22 10:05:20 +0200

    2 files modified

--- server.js
+++ server.js
@@ -1,3 +1,49 @@
+
+const https = require('https');
+
+let url ='https://www.yr.no/api/v0/locations/2-3083828/forecast'; // Dąbie
+//let url ='https://www.yr.no/api/v0/locations/5-1220500/forecast'; // Szczecin 
+
+const parsowanie=(body)=>{
+  //console.log(body);
+		for (var o in body){
+			//console.log(o);
+		}
+    console.log("------------------------------");
+		for (var o in body.longIntervals){
+      let day = body.longIntervals[o];
+			//console.log(o,day);
+      
+      let czas = (new Date(day.start)).toLocaleString('pl-PL');
+      let deszcz = Math.max(day.precipitation.value,day.precipitation.min,day.precipitation.max);
+      console.log(czas,"T=",day.temperature.value,"FL=",day.feelsLike.value,"  D="+deszcz+" mm");
+      
+		}
+
+}
+
+https.get(url,(res) => {
+    let body = "";
+    res.on("data", (chunk) => {
+        body += chunk;
+    });
+    res.on("end", () => {
+        try {
+            let json = JSON.parse(body);
+            //console.log(json);
+            parsowanie(json);
+        } catch (error) {
+            console.error(error.message);
+        };
+    });
+}).on("error", (error) => {
+    console.error(error.message);
+});
+
+
+
+
+
 var http = require('http');
 
 http
