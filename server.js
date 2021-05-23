9829cd8e31b52ac015457c8c1d484dfee229d423
ZnakZorro <znakzorro@gmail.com>
2021-05-23 07:58:30 +0200

    1 file modified

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ let serverStart=(html="qwerty")=>{
       res.write(czas+html+' <br />yrno Hello, Node.js!'); //write a response to the client
       res.end(); //end the response
     })
-    .listen(8088); //the server object listens on port 8080
+    .listen(8089); //the server object listens on port 8080
 
   console.log('Server running on port 8088');
 }
